a8573c9e78c5dcaff2da6a10a37b9daf56213c70
thzois <thzois.csd@gmail.com>
2020-02-28 10:50:48 +0100

    clean data -start

--- data-analysis/clean_data.py
+++ data-analysis/clean_data.py
@@ -1,7 +1,10 @@
-import pandas as pd
+import pycountry
 import json
 
-
+# // - Same tweets from the same user
+# // - location = null
+# // - discard neutral after sentiment analysis - try without removing them
+# // - check if a person tweets more than 3 times in a certain time period. He shouldn't tweet every 10 seconds for example because that's a bot
 
 # read events.json 
 def read_events():
@@ -9,16 +12,63 @@ def read_events():
         return json.load(read_file)
 
 
-def clean_data(events):
+def suppress_data(events):
     for event in events["events"]:
         event_file = event["start_date"] + "_" + event["end_date"] + ".json"
         with open("tweets/" + event_file, 'r') as read_file:
             # list (array) of tweets
-            tweets = json.load(read_file)["tweets"]
+            full_tweets = json.load(read_file)["tweets"]
             
-            # here clean data for ONE specific event
-            print("I am cleaning data for", event_file, "which has", len(tweets), "tweets")
-            
+            mapping = {country.name: country.alpha_2 for country in pycountry.countries}
+            # iterrate the tweets for each event
+            tweets = { "tweets": [] }
+            for t in full_tweets:    
+                
+                if t["user"]["location"] != None:
+                    try:
+                        location = [t["user"]["location"]]
+                        standard_name = cc.convert(names = location, to = 'name_short')
+                        print(standard_name, " - ", t["user"]["location"])
+                    except:
+                        print("None - ", t["user"]["location"])
+                    
+                # if t["user"]["location"] not None:
+                #     tweet = {}
+                #     tweet["created_at"] = t["created_at"]
+                #     tweet["user_id"] = t["user"]["id"]
+                #     tweet["user_location"]= t["user"]["location"]
+                #     tweet["user_followers"] = t["user"]["followers_count"]
+
+
+
+                #     if "extended_tweet" in t:
+                #         try:
+                #             tweet = t["extended_tweet"]["full_text"]
+                #         except:
+                #             tweet = t["text"]
+                #     elif "retweeted_status" in t:
+                #         try:
+                #             tweet = t["retweeted_status"]["extended_tweet"]["full_text"]
+                #         except:
+                #             tweet = t["retweeted_status"]["text"]
+                #     else:
+                #         tweet = t["text"]
+
+                #     tweets["tweets"].append(tweet)
+                #     # if "retweeted_status" in t:
+                #     #     try:
+                #     #         tweet = t["retweeted_status"]["extended_tweet"]["full_text"]
+                #     #     except:
+                #     #         tweet = t["retweeted_status"]["text"]
+                #     # else:
+                #     #     try:
+                #     #         tweet = t["extended_tweet"]["full_text"]
+                #     #     except:
+                #     #         tweet = t["text"]
+
+                
+        break
+
 
         # TODO: Remove comments to store cleaned data in another directory
         # later we will perform sentiment analysis
@@ -29,9 +79,8 @@ def clean_data(events):
 
 
 def main():
-    # events = read_events()
-    # clean_data(events)
-    print("CLEAN DATA")
+    events = read_events()
+    suppress_data(events)
 
 
 if __name__ == "__main__":
