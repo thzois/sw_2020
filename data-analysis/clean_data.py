2e728708ef9fc0b18d2802982cdf15394227287e
thzois <thzois.csd@gmail.com>
2020-02-29 14:47:08 +0100

    testing clean data

--- data-analysis/clean_data.py
+++ data-analysis/clean_data.py
@@ -3,10 +3,7 @@ import pycountry
 import json
 import re
 
-# // - Same tweets from the same user
-# // - location = null
-# // - discard neutral after sentiment analysis - try without removing them
-# // - check if a person tweets more than 3 times in a certain time period. He shouldn't tweet every 10 seconds for example because that's a bot
+
 
 # read events.json 
 def read_events():
@@ -29,7 +26,7 @@ def search_location(location):
     return country
 
 
-def suppress_data(events):
+def clean_data(events):
     for event in events["events"]:
         event_file = event["start_date"] + "_" + event["end_date"] + ".json"
         with open("tweets/" + event_file, 'r') as read_file:
@@ -37,51 +34,63 @@ def suppress_data(events):
             full_tweets = json.load(read_file)["tweets"]
             # iterrate the tweets for each event
             tweets = { "tweets": [] }
-            count_undiscovered = 0
+
+            # statistics
+            total_tweets = len(full_tweets)
+            total_tweets_loc = 0
+            total_tweets_loc_undesc = 0
+            
             for t in full_tweets:
                 if t["user"]["location"] != None:
-                    location = re.split('; |- |,', t["user"]["location"])
-                    country = search_location(location)
+                    # total_tweets_loc += 1
+                    # location = re.split('; |- |,', t["user"]["location"])
+                    # country = search_location(location)
 
-                    if not country:
-                        location = t["user"]["location"].split(' ')
-                        country = search_location(location)
-                        if not country:
-                            print(t["user"]["location"])
-                            count_undiscovered += 1
-                    if country:
-                        tweet = {
-                            "created_at": datetime.strptime(t["created_at"], "%a %b %y %H:%M:%S %z %Y").strftime("%Y-%m-%d %H:%M:%S"),
-                            "user_id": t["user"]["id"],
-                            "user_location": {
-                                'alpha_2': country[0].alpha_2,
-                                'alpha_3': country[0].alpha_3,
-                                'name': country[0].name,
-                                'numeric': country[0].numeric,
-                                'official_name': country[0].official_name if hasattr(country[0],'official_name') else None
-                            },
-                            "user_followers": t["user"]["followers_count"],
-                            'is_duplicate': False
-                        }
+                    # if not country:
+                    #     location = t["user"]["location"].split(' ')
+                    #     country = search_location(location)
+                    #     if not country:
+                    #         total_tweets_loc_undesc += 1
+                    # if country:
+                    #     tweet = {
+                    #         "created_at": datetime.strptime(t["created_at"], "%a %b %y %H:%M:%S %z %Y").strftime("%Y-%m-%d %H:%M:%S"),
+                    #         "user_id": t["user"]["id"],
+                    #         "user_location": {
+                    #             'alpha_2': country[0].alpha_2,
+                    #             'alpha_3': country[0].alpha_3,
+                    #             'name': country[0].name,
+                    #             'numeric': country[0].numeric,
+                    #             'official_name': country[0].official_name if hasattr(country[0],'official_name') else None
+                    #         },
+                    #         "user_followers": t["user"]["followers_count"],
+                    #         'is_duplicate': False
+                    #     }
 
-                        if "retweeted_status" in t:
-                            try:
-                                tweet["text"] = t["retweeted_status"]["extended_tweet"]["full_text"]
-                            except:
-                                tweet["text"] = t["retweeted_status"]["text"]
-                        else:
-                            try:
-                                tweet["text"] = t["extended_tweet"]["full_text"]
-                            except:
-                                tweet["text"] = t["text"]
+                    #     if "extended_tweet" in t:
+                    #         try:
+                    #             tweet["text"] = t["extended_tweet"]["full_text"]
+                    #         except:
+                    #             tweet["text"] = t["text"]
+                    #     else if "retweeted_status" in t:
+                    #         try:
+                    #             tweet["text"] = t["retweeted_status"]["extended_tweet"]["full_text"]
+                    #         except:
+                    #             tweet["text"] = t["retweeted_status"]["text"]
+                    #     else:
+                    #         tweet["text"] = t["text"]
 
-                        tweets["tweets"].append(tweet)
-                        print(count_undiscovered)
+                    ### testing start
+                    tweet = {
+                        "created_at": datetime.strptime(t["created_at"], "%a %b %y %H:%M:%S %z %Y").strftime("%Y-%m-%d %H:%M:%S"),
+                        "user_id": t["user"]["id"],
+                        "user_followers": t["user"]["followers_count"],
+                        'is_duplicate': False
+                    }
+                    ### testing end
 
-        # TODO: Remove comments to store cleaned data in another directory
-        # later we will perform sentiment analysis
-        # store the cleaned data
-    
+                    tweets["tweets"].append(tweet)
+                    
+
         tweets_per_user = {}
         for tweet in tweets['tweets']:
             user_tweets = tweets_per_user.get(tweet['user_id'])
@@ -91,59 +100,62 @@ def suppress_data(events):
                     'is_bot': False
                     }})
             else:
-                user_tweets.append(tweet)
+                print(user_tweets)
+                user_tweets['tweets'].append(tweet)
                 tweets_per_user.tweet['user_id']['tweets'].append(tweet)
+        print(tweets_per_user)
 
-        # Sort by date
-        for user_data in tweets_per_user:
-            user_data['tweets'].sort(key=lambda x: datetime.strptime(x['created_at'], "%Y-%m-%d %H:%M:%S"))
+        # # Sort by date
+        # for user_data in tweets_per_user:
+        #     user_data['tweets'].sort(key=lambda x: datetime.strptime(x['created_at'], "%Y-%m-%d %H:%M:%S"))
 
-        # Identify bots
+        # # Identify bots
+        # for user_data in tweets_per_user:
+        #         tweet_date = None
+        #         times_found_in_30_sec = 0
+        #         for tweet in user_data['tweets']:
+        #             if not tweet_date:
+        #                 tweet_date = tweet['created_at']
+        #                 continue
+        #             if tweet_date and (tweet['created_at'] - tweet_date).seconds < 10:
+        #                 times_found_in_30_sec += 1
+        #             if times_found_in_30_sec == 3:
+        #                 user_data['is_bot'] = True
 
-        for user_data in tweets_per_user:
-                tweet_date = None
-                times_found_in_30_sec = 0
-                for tweet in user_data['tweets']:
-                    if not tweet_date:
-                        tweet_date = tweet['created_at']
-                        continue
-                    if tweet_date and (tweet['created_at'] - tweet_date).seconds < 10:
-                        times_found_in_30_sec += 1
-                    if times_found_in_30_sec == 3:
-                        user_data['is_bot'] = True
+        # # remove bots
+        # tweets_per_user_no_bots = {}
+        # for user_id, user_data in tweets_per_user.items():
+        #     if user_data['is_bot']:
+        #         continue
+        #     tweets_per_user_no_bots.update({user_id: user_data})
 
-        # remove bots
-        tweets_per_user_no_bots = {}
-        for user_id, user_data in tweets_per_user.items():
-            if user_data['is_bot']:
-                continue
-            tweets_per_user_no_bots.update({user_id: user_data})
+        # # identify duplicate tweets per user
+        # for user_tweets in tweets_per_user:
+        #     for i in range(0, user_tweets.length - 1):
+        #         for j in range(i + 1, user_tweets.length):
+        #             if user_tweets[i]['text'] == user_tweets[j]['text']:
+        #                 user_tweets['is_duplicate'] = True
 
-        # identify duplicate tweets per user
-        for user_tweets in tweets_per_user:
-            for i in range(0, user_tweets.length - 1):
-                for j in range(i + 1, user_tweets.length):
-                    if user_tweets[i]['text'] == user_tweets[j]['text']:
-                        user_tweets['is_duplicate'] = True
+        # tweets_per_user_final = {}
+        # for user_id, user_data in tweets_per_user.items():
+        #     for tweet in user_data['tweets']:
+        #         if tweet['is_duplicate']:
+        #             continue
+        #         if tweets_per_user_final.get(user_id):
+        #             tweets_per_user_final[user_id]['tweets'].append(tweet)
+        #         else:
+        #             tweets_per_user_final.update({user_id: {'tweets': [tweet]}})
 
-        tweets_per_user_final = {}
-        for user_id, user_data in tweets_per_user.items():
-            for tweet in user_data['tweets']:
-                if tweet['is_duplicate']:
-                    continue
-                if tweets_per_user_final.get(user_id):
-                    tweets_per_user_final[user_id]['tweets'].append(tweet)
-                else:
-                    tweets_per_user_final.update({user_id: {'tweets': [tweet]}})
 
-        if len(tweets["tweets"]) > 0:
-            with open("tweets/cleaned/" + event_file, 'w') as outfile:
-                json.dump(tweets, outfile, ensure_ascii=True, indent=4)
+
+        # if len(tweets["tweets"]) > 0:
+        #     with open("tweets/cleaned/" + event_file, 'w') as outfile:
+        #         json.dump(tweets, outfile, ensure_ascii=True, indent=4)
 
 
 def main():
     events = read_events()
-    suppress_data(events)
+    clean_data(events)
 
 
 if __name__ == "__main__":
