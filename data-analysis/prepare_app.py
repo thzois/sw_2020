66de72807afbc3307b1b80d1f0044d14cd04d50c
Bogdan Ene <b.ene@nelson.nl>
2020-03-04 01:14:22 +0100

    small update after merge

--- data-analysis/prepare_app.py
+++ data-analysis/prepare_app.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+from operator import itemgetter
+
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
@@ -103,21 +106,23 @@ def stock_sent_day_normalized(events):
 def prepare_barchart_data(events):
     for event in events["events"]:
         filename = event["start_date"] + "_" + event["end_date"]
-        # stock_df.to_csv('../web-app/results/stocks/' + filename, index = False)
         with open(f"tweets/results/{filename}.json", "r") as twitter_file:
             twitter_data = json.load(twitter_file)["tweets"]
-            sentiments_per_country = {}
+            tweets_per_country = {}
             for t in twitter_data:
                 country_name = t['user_location']['name']
-                if sentiments_per_country.get(country_name):
-                    sentiments_per_country[country_name] += 1
+                if tweets_per_country.get(country_name):
+                    tweets_per_country[country_name] += 1
                 else:
-                    sentiments_per_country.update(
+                    tweets_per_country.update(
                         {
                             country_name: 1
                         })
+            sorted_tweets_per_country = OrderedDict(
+                sorted(tweets_per_country.items(), key=itemgetter(1), reverse=True))
+
             with open(f"tweets/results/barchart_{filename}.json", 'w') as outfile:
-                json.dump(sentiments_per_country, outfile, ensure_ascii=True, indent=4)
+                json.dump(sorted_tweets_per_country, outfile, ensure_ascii=True, indent=4)
 
 
 
